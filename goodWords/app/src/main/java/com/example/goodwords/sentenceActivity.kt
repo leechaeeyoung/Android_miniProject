c5488797ba64fff7dec0656c50c32eb27b4ced6a
leechaeyoung <zs1397@naver.com>
2023-01-10 13:39:16 +0900

    환경설정

---
+++ goodWords/app/src/main/java/com/example/goodwords/sentenceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.goodwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sentenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentence)
+    }
+}
